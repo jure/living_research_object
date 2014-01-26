67161b8f8c0851eda5aea8b632ec331e0ca233e0
Jure Triglav <juretriglav@gmail.com>
2014-01-26 13:27:55 +0100

    Initial commit

---
+++ create_database.rb
@@ -0,0 +1,41 @@
+require 'sqlite3'
+require 'json'
+require 'pry'
+
+db = SQLite3::Database.new('experiment-snapshot.sqlite')
+db_result = SQLite3::Database.new('experiment-result.sqlite')
+
+tweets = db.execute('SELECT * FROM tweets')
+weather = db.execute('SELECT * FROM weather')
+
+db_result.execute("CREATE TABLE tweets (timestamp INTEGER, tweet TEXT)")
+db_result.execute("CREATE TABLE clouds (timestamp INTEGER PRIMARY KEY UNIQUE, coverage INTEGER)")
+db_result.execute("CREATE TABLE metadata (id INTEGER, tweets INTEGER, clouds INTEGER)")
+
+weather.map!{|w| JSON.parse(w.first)}
+weather_dedup = []
+weather.each do |w|
+  unless weather_dedup.index(w)
+    weather_dedup << w
+  end
+end
+
+weather_dedup.each do |w|
+  db_result.execute('INSERT INTO clouds VALUES (?,?)', w['dt'], w['clouds']['all'])
+end
+
+total = tweets.size
+
+db_result.execute('INSERT INTO metadata VALUES (?,?,?)', 1, total, weather_dedup.size)
+
+tweets.each_with_index do |t,i|
+  t = JSON.parse(t[0]) 
+  time = DateTime.parse(t['created_at']).to_time.to_i
+
+  next if t['text'] =~ /forecast/i # Skip if tweet is a forecast
+  if t['text'] =~ /\b(sunny|☀|soleado|sunshine|太陽|sunlight|#sun)\b/i
+    db_result.execute('INSERT INTO tweets VALUES (?,?)', time, t['text'])
+  end
+  puts "#{i} of #{total}"
+end
+
---
+++ server.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var Twit = require('twit');
+var io = require('socket.io').listen(server);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('experiment-result-old.sqlite');
+var db_archive = new sqlite3.Database('experiment-archive.sqlite');
+
+var request = require('request');
+
+db.run("CREATE TABLE IF NOT EXISTS tweets (timestamp INTEGER, tweet TEXT)");
+db.run("CREATE TABLE IF NOT EXISTS clouds (timestamp INTEGER PRIMARY KEY UNIQUE, coverage INTEGER)");
+db_archive.run("CREATE TABLE IF NOT EXISTS tweets (data TEXT)");
+db_archive.run("CREATE TABLE IF NOT EXISTS weather (data TEXT)");
+
+setInterval(function() {
+    request('http://api.openweathermap.org/data/2.5/weather?id=5391959&units=metric', function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body);
+            var data = JSON.parse(body);
+            if (data !== null && data.clouds !== undefined) {
+                db_archive.run("INSERT INTO weather VALUES(?)", JSON.stringify(data));
+                db.get("SELECT * FROM clouds WHERE timestamp = ?", data.dt, function(err, row) {
+                    if (row === undefined) {
+                        db.run("UPDATE metadata SET clouds=clouds + 1 WHERE id = 1");
+                        db.run("INSERT OR IGNORE INTO clouds VALUES(?,?)", data.dt, data.clouds.all);
+                        io.sockets.emit("clouds", [data.dt, data.clouds.all]);
+                    }
+                });
+
+                console.log("Clouds are: " + data.clouds.all);
+            }
+        }
+    });
+}, 300 * 1000);
+
+server.listen(8080);
+
+var locations = ['-122.75', '36.8', '-121.75', '37.8'];
+
+var T = new Twit({
+    consumer_key: process.env.LRO_TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.LRO_TWITTER_CONSUMER_SECRET,
+    access_token: process.env.LRO_TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.LRO_TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var stream = T.stream('statuses/filter', {
+    locations: locations
+});
+
+stream.on('tweet', function(tweet) {
+    var timestamp = new Date(Date.parse(tweet.created_at)).getTime() / 1000;
+    io.sockets.emit('processed', [timestamp, tweet.text]);
+    db_archive.run("INSERT INTO tweets VALUES(?)", JSON.stringify(tweet));
+    if (tweet.text.match(/\b(sunny|☀|soleado|sunshine|太陽|sunlight|#sun)\b/i)) {
+        io.sockets.emit('tweet', [timestamp, tweet.teaxt]);
+        db.run("UPDATE metadata SET tweets=tweets + 1 WHERE id = 1");
+        db.run("INSERT INTO tweets VALUES(?,?)", timestamp, tweet.text);
+        console.log(tweet.text);
+    }
+});
+io.sockets.on('connection', function(socket) {
+    var clouds = [];
+    var tweets = [];
+
+    db.each("SELECT * FROM tweets", function(err, row) {
+        tweets.push(row);
+    }, function() {
+        db.each("SELECT * FROM clouds", function(err, row) {
+            clouds.push(row);
+        }, function() {
+            var current = {
+                clouds: clouds,
+                tweets: tweets
+            };
+
+            io.sockets.emit('current', JSON.stringify(current));
+            console.log('Connected to client');
+        });
+    });
+});
